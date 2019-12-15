af4b681d0ee06dcba7e6929c4801186d61f5d859
Andrew Simmons <agsimmons0@gmail.com>
2019-12-14 19:58:26 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 from setuptools import setup, find_packages
 
 setup(
-    name='sortphotos',
-    version='1.0',
-    description='Organizes photos and videos into folders using date/time information ',
-    author='Andrew Ning',
+    name="sortphotos",
+    version="1.0.0",
+    description="Organizes photos and videos into folders using date/time information ",
+    author="Andrew Ning",
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',
-    entry_points={
-        'console_scripts': [
-          'sortphotos = src.sortphotos:main',
-        ]
-      }
+    license="MIT License",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={"console_scripts": ["sortphotos = sortphotos.sortphotos:main",]},
 )
-
